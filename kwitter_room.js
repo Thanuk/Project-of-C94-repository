ea0528afdef3116d254e004d646f73955dcc00b8
Thanuk <86400849+Thanuk@users.noreply.github.com>
2021-12-25 19:41:28 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,17 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+    };
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
